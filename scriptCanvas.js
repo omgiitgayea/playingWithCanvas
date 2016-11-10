841430abf3dad80d76660da874e2acb27f6475ca
Christopher Rescorla <cgrescorla@gmail.com>
2016-11-10 07:43:00 -0700

    tiny change

--- scriptCanvas.js
+++ scriptCanvas.js
@@ -6,12 +6,11 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 150 , 75);
 
-var ltx = canvas.getContext("2d");
-ltx.moveTo(0, 0);
-ltx.lineTo(400, 200);
-ltx.stroke();
+ctx.moveTo(0, 0);
+ctx.lineTo(400, 200);
+ctx.stroke();
 
-var etx = canvas.getContext("2d");
-etx.beginPath();
-etx.arc(95, 50, 40, 0, 2*Math.PI);
-etx.stroke()
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2*Math.PI);
+ctx.stroke();
+
