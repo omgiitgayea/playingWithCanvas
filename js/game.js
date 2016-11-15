977fc2534b8083b78df9184b492c365ca13d426d
Christopher Rescorla <cgrescorla@gmail.com>
2016-11-15 09:54:05 -0700

    animation added, plus gravity and click event

--- js/game.js
+++ js/game.js
@@ -6,9 +6,17 @@ var
     canvas,
     renderingContext,
     width,
-    height;
+    height,
+    dx;
 
-var frames = 0;
+var frames = 0,
+    currentSprite = 0,
+    dy = 0,
+    currentY = 50;
+
+var FRAMES_PER_IMAGE = 5,
+    GRAVITY = 5,
+    UPSPEED = 15;
 
 function main()
 {
@@ -46,23 +54,20 @@ function windowSetup()
     }
 
     // Create a listener on the input event
-    // document.addEventListener(inputEvent, onpress);
+    document.addEventListener(inputEvent, onpress);
 }
 
 function loadGraphics()
 {
     // initiate the sprite sheet
     var img = new Image();
-    img.src = "images/massEffectSpriteSheet.png";       // guess what, the ME sheet doesn't have transparency
+    img.src = "images/massEffectSpriteSheet.png";
     img.onload = function ()
     {
         initSprites(this);
         charSprite[0].draw(renderingContext, 50, 50);
         gameLoop();
     };
-
-
-
 }
 
 function gameLoop()
@@ -75,11 +80,49 @@ function gameLoop()
 
 function update()
 {
-
     frames++;
+    if (frames % FRAMES_PER_IMAGE === 0) {
+        if (currentSprite < charSprite.length - 1 || currentSprite === 0) {
+            currentSprite++;
+        }
+        else {
+            currentSprite--;
+        }
+
+        if (currentY + charSprite[currentSprite].height < height)
+        {
+            currentY += GRAVITY;
+        }
+        else
+        {
+            currentY = height - charSprite[currentSprite].height;
+        }
+        if (currentY < 0)
+        {
+            UPSPEED = 0;
+            GRAVITY = 50;
+        }
+    }
 }
 
 function render()
 {
 
+    renderingContext.clearRect(0, 0, width, height);
+    if ((UPSPEED === 0 && (currentY + charSprite[currentSprite].height >= height)) || (currentY + charSprite[currentSprite].height >= height))
+    {
+        charSprite[0].draw(renderingContext, 50, height - charSprite[0].height);
+        UPSPEED = 0;
+    }
+    else
+    {
+        charSprite[currentSprite].draw(renderingContext, 50, currentY);
+    }
+
+}
+
+function onpress()
+{
+    currentY -= UPSPEED;
+    // render();
 }
